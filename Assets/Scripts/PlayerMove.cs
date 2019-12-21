f51f589ba7979b480600e1760c33026837d7a36b
WindowsBuild <46250151+WindowsBuild@users.noreply.github.com>
2019-12-20 17:36:15 -0800

    Player movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hmove = Input.GetAxis("Horizontal");
+        float Vmove = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(Hmove,Vmove);
+
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
