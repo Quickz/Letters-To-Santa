80384ef5d5c9eb90e4d4c85c0f59dd4413566aa0
Quickz <alexserg@inbox.lv>
2019-12-21 04:55:29 +0200

    implemented basic UI workflow

---
+++ Assets/Scripts/UI/CreditsScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class CreditsScreen : BaseScreen
+{
+    public static CreditsScreen Instance { get; private set; }
+
+    [SerializeField]
+    private Button backButton = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate screen detected");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(MainScreen.Instance.OpenAlone);
+    }
+}
---
+++ Assets/Scripts/UI/MainScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public sealed class MainScreen : BaseScreen
+{
+    public static MainScreen Instance { get; private set; }
+
+    [SerializeField]
+    private Button startButton = null;
+
+    [SerializeField]
+    private Button highscoresButton = null;
+
+    [SerializeField]
+    private Button creditsButton = null;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate screen detected");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        highscoresButton.onClick.AddListener(HighscoresScreen.Instance.OpenAlone);
+        creditsButton.onClick.AddListener(CreditsScreen.Instance.OpenAlone);
+
+        OpenAlone();
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
