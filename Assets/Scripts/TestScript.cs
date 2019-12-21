e7218bfcc32253a0dc97916e7a7a0d6f994bdd6f
Quickz <alexserg@inbox.lv>
2019-12-21 05:26:54 +0200

    made a test movement script

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private new Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+
+        rigidbody.velocity = new Vector2(moveX * speed, moveY * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+    }
+}
