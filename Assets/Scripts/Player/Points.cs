0b42c26b37f58267fcbd25bfc993a6e217d58f64
WindowsBuild <46250151+WindowsBuild@users.noreply.github.com>
2019-12-21 02:03:15 -0800

    Added a timer
    Also added a temp background for the menu. also some script clean up.

--- Assets/Scripts/Player/Points.cs
+++ Assets/Scripts/Player/Points.cs
@@ -9,10 +9,7 @@ public class Points : MonoBehaviour
     public int point;
     public int Total;
     public Text tx;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public WinScreen ws;
+    public Points pt;
+    public Text tx;
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLeft -= Time.deltaTime;
+        if (TimeLeft <= 0 && pt.point >= 50) {
+            ws.Open();
+        }
+
+        tx.text = "" + (int)TimeLeft;
+
+        if (TimeLeft <= 0 && pt.point <= 50) {
+            TimeLeft = 0;
+        }
+    }
+}
