f45c4e1d0a50aef473e4812e05c9774570f1a16d
WindowsBuild <46250151+WindowsBuild@users.noreply.github.com>
2019-12-20 18:08:24 -0800

    Grabbing

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public bool isGrabbed;
+    RaycastHit2D ray;
+    public float distance;
+    public Transform point;
+    public Transform HoldPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+
+            if (!isGrabbed)
+            {
+                ray = Physics2D.Raycast(point.position, Vector2.right * transform.localScale.x, distance);
+
+                if (ray.collider != null)
+                {
+                    isGrabbed = true;
+                }
+
+            }
+            else
+            {
+                ray.collider.gameObject.transform.position = HoldPoint.position;
+            }
+        }
+    }
+}
