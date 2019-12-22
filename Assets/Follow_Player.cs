624664fc47879c5d97557f2b8f6cfa4efa317d4e
JWoodrell <Joseph@Woodrell.com>
2019-12-21 22:17:41 -0500

    Update Follow_Player.cs
    changed camera to use FixedUpdate and took out some debug stuff

--- Assets/Follow_Player.cs
+++ Assets/Follow_Player.cs
@@ -7,10 +7,6 @@ public class Follow_Player : MonoBehaviour
     public GameObject Player;
     Vector3 CamPosition;
     Vector3 CamError;
-    [SerializeField]
-    float CamErrorMag;
-    [SerializeField]
-    private float CamSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +15,10 @@ public class Follow_Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CamError = Player.transform.position - CamPosition;
-        CamErrorMag = CamError.magnitude;
-        CamPosition += (CamError) * Time.deltaTime * (CamErrorMag - 9);
+        CamPosition += (CamError) * Time.fixedDeltaTime * (CamError.magnitude - 9);
         CamPosition.z = -10;
         gameObject.transform.position = CamPosition;
     }
