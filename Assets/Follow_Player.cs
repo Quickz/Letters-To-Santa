4cc4724eef78317db7f6b4afe04a9f3830107213
JWoodrell <Joseph@Woodrell.com>
2019-12-21 22:06:22 -0500

    snow menu (old) and cam follow

---
+++ Assets/Follow_Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Player : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 CamPosition;
+    Vector3 CamError;
+    [SerializeField]
+    float CamErrorMag;
+    [SerializeField]
+    private float CamSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamError = Player.transform.position - CamPosition;
+        CamErrorMag = CamError.magnitude;
+        CamPosition += (CamError) * Time.deltaTime * (CamErrorMag - 9);
+        CamPosition.z = -10;
+        gameObject.transform.position = CamPosition;
+    }
+}
---
+++ Assets/SnowFall_Top.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFall_Top : MonoBehaviour
+{
+    public Vector3 SnowPos;
+    public Vector3 SnowStartPos;
+    public Vector3 SnowOffset;
+    public float Speed;
+    public Color SnowColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SnowStartPos = new Vector2(700 + (Random.value * 50.0f), 700+(Random.value * 50.0f));
+        Speed = 80.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SnowPos.y < -100.0f)
+        {
+            SnowOffset += SnowStartPos - SnowPos;
+        }
+        SnowPos.x = SnowStartPos.x - (Speed) * Time.time - (2.0f * Speed) * Mathf.PerlinNoise(0.5f * Time.time + SnowStartPos.x, -0.5f * Time.time + SnowStartPos.y) + SnowOffset.x;
+        SnowPos.y = SnowStartPos.y - (2.0f * Speed) * Time.time - Speed * Mathf.PerlinNoise(Time.time, Time.time) + SnowOffset.y;
+        gameObject.GetComponent<RectTransform>().position = SnowPos;
+    }
+}
