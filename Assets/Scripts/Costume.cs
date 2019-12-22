eedbe4f5849d0b6fd7a44b5038507652dbb94d9b
Quickz <alexserg@inbox.lv>
2019-12-22 06:43:23 +0200

    implemented scriptable object related code that for the effects and costumes

---
+++ Assets/Scripts/Costume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "New Costume",
+    menuName = "Scripable Object/Costume")]
+public class Costume : ScriptableObject
+{
+    public Sprite FrontSprite => FrontSprite;
+
+    public ReadOnlyCollection<Effect> Effects => effects.AsReadOnly();
+
+    [SerializeField]
+    private Sprite frontSprite = null;
+
+    [SerializeField]
+    private List<Effect> effects = null;
+}
