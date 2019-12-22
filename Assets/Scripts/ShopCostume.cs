c5e73988d85e95b5181a8576ad903afdbeca49ce
Quickz <alexserg@inbox.lv>
2019-12-23 01:24:57 +0200

    added a shop screen with a couple of costumes and wrote some logic regarding that

---
+++ Assets/Scripts/ShopCostume.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopCostume : MonoBehaviour
+{
+    public Costume Costume => costume;
+
+    [SerializeField]
+    private Costume costume = null;
+
+    [SerializeField]
+    private Button buyButton = null;
+
+    [SerializeField]
+    private Inventory playerInventory = null;
+
+    [SerializeField]
+    private Image icon = null;
+
+    [SerializeField]
+    private TMP_Text priceLabel = null;
+
+    private void Start()
+    {
+        if (playerInventory.costumes.Contains(costume))
+        {
+            DisablePurchasingOption();
+        }
+        else
+        {
+            buyButton.onClick.AddListener(Buy);
+        }
+    }
+
+    private void Buy()
+    {
+        if (CoinManager.Coins < costume.Price)
+        {
+            return;
+        }
+
+        CoinManager.Coins -= costume.Price;
+        playerInventory.costumes.Add(costume);
+        DisablePurchasingOption();
+    }
+
+    private void DisablePurchasingOption()
+    {
+        buyButton.interactable = false;
+        TMP_Text textField = buyButton.GetComponentInChildren<TMP_Text>();
+        textField.text = "Owned";
+        textField.color = Color.white;
+    }
+
+    private void OnValidate()
+    {
+        if (costume != null)
+        {
+            if (icon != null && icon.sprite != costume.FrontSprite)
+            {
+                icon.sprite = costume.FrontSprite;
+            }
+
+            string price = $"{costume.Price}c";
+            if (priceLabel != null && priceLabel.text != price)
+            {
+                priceLabel.text = price;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ShopScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class ShopScreen : BaseScreen
+{
+    public static ShopScreen Instance { get; private set; }
+
+    [SerializeField]
+    private Inventory playerInventory = null;
+
+    [SerializeField]
+    private Inventory availableCostumes = null;
+
+    [SerializeField]
+    private Button backButton = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate screen detected");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(MainScreen.Instance.OpenAlone);
+
+        playerInventory.costumes.Clear();
+
+        // default costume
+        playerInventory.costumes.Add(availableCostumes.costumes[0]);
+    }
+}
