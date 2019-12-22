9c455297bc732f3d718a890ce140bc5dc8b0b863
WindowsBuild <46250151+WindowsBuild@users.noreply.github.com>
2019-12-22 15:03:51 -0800

    Tutorial Script

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] popUps;
+    private int popUpIndex;
+    public float WaitTime = 2f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else {
+                popUps[i].SetActive(false);
+            }
+
+            if (popUpIndex == 0) {
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) {
+                    popUpIndex++;
+                }
+                else if(popUpIndex == 1){
+                    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) {
+                        popUpIndex++;
+                    }
+                }
+                else if (popUpIndex == 1)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        popUpIndex++;
+                    }
+                }
+            }
+        }
     }
 }
