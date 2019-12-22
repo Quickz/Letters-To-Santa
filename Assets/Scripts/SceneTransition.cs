09c2657c166432700c18ee55d42d728d661359a8
Quickz <alexserg@inbox.lv>
2019-12-22 03:54:56 +0200

    re-added previously erased changes and made sure they work - animations, transitional effects etc.

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : Transition
+{
+    public static SceneTransition Instance { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
