13fc763e7629349689f697683187ebaf40a61fd3
Quickz <alexserg@inbox.lv>
2019-12-23 03:41:52 +0200

    made the level selection level buttons load the scene that's based on a string that's specified on the button

--- Assets/Scripts/LevelSelectionScreen.cs
+++ Assets/Scripts/LevelSelectionScreen.cs
@@ -11,15 +11,6 @@ public class LevelSelectionScreen : BaseScreen
     [SerializeField]
     private Button backButton = null;
 
-    [SerializeField]
-    private Button levelButton1 = null;
-
-    [SerializeField]
-    private Button levelButton2 = null;
-
-    [SerializeField]
-    private Button levelButton3 = null;
-
     protected override void Awake()
     {
         base.Awake();
@@ -38,18 +29,5 @@ public class LevelSelectionScreen : BaseScreen
     private void Start()
     {
         backButton.onClick.AddListener(MainScreen.Instance.OpenAlone);
-        levelButton1.onClick.AddListener(OpenNextSceneWithFade);
-        levelButton2.onClick.AddListener(OpenNextSceneWithFade);
-        levelButton3.onClick.AddListener(OpenNextSceneWithFade);
-    }
-
-    private void OpenNextSceneWithFade()
-    {
-        SceneTransition.Instance.RunWithFade(OpenNextScene);
-    }
-
-    private void OpenNextScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
