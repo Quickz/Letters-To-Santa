584a74abe330bb362e9cf3eee49302b145c16512
JWoodrell <Joseph@Woodrell.com>
2019-12-22 20:49:38 -0500

    3rd level for grab edits

--- Assets/Scripts/Bags/Spawner3.cs
+++ Assets/Scripts/Bags/Spawner3.cs
@@ -15,8 +15,8 @@ public class Spawner3 : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer <= 0) {
             randomLetter = Random.Range(1, 3);
-            Pos.x = (Random.value * 22.0f) - 11.0f;
-            Pos.y = 0 - (Random.value * 7f);
+            Pos.x = -2 + (Random.value * 12.0f);
+            Pos.y = -4 - (Random.value * 3.0f);
             Pos.z = 0;
             Instantiate(Letters[randomLetter], Pos, Quaternion.identity);
             timer = 5;
