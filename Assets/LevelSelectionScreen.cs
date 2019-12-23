9572017704c064045e99fc910ed758448f72d079
Quickz <alexserg@inbox.lv>
2019-12-23 04:10:11 +0200

    fixed level selection screen

---
+++ Assets/LevelSelectionScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectionScreen : BaseScreen
+{
+    public static LevelSelectionScreen Instance { get; private set; }
+
+    [SerializeField]
+    private Button backButton = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Duplicate screen detected");
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(MainScreen.Instance.OpenAlone);
+    }
+}
