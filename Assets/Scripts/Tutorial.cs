0ff7a6b119c88cd4c1176f212d8de9232ce3bea2
WindowsBuild <46250151+WindowsBuild@users.noreply.github.com>
2019-12-22 17:17:52 -0800

    Tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,17 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Tutorial : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popUpIndex;
-    public float WaitTime = 2f;
+    public float WaitTime = 10f;
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    public float typingSpeed;
+    public int index;
+    public float thing = 3;
 
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type() {
+        foreach (char letter in sentences[index].ToCharArray()) {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentince() {
+        thing -= Time.deltaTime;
+        if (thing <= 0)
+        {
+            if (index < sentences.Length - 1)
+            {
+                index++;
+                textDisplay.text = " ";
+                StartCoroutine(Type());
+            }
+            else
+            {
+                textDisplay.text = " ";
+            }
+            thing = 10;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
+        NextSentince();
         for (int i = 0; i < popUps.Length; i++)
         {
             if (i == popUpIndex)
@@ -29,7 +67,9 @@ public class Tutorial : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.A))
             {
                 popUpIndex++;
+               
             }
+          
         }
         else if (popUpIndex == 1)
         {
@@ -37,13 +77,16 @@ public class Tutorial : MonoBehaviour
             {
                 popUpIndex++;
             }
+    
         }
         else if (popUpIndex == 2)
         {
+
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
             {
                 popUpIndex++;
             }
+            NextSentince();
         }
         else if (popUpIndex == 3)
         {
@@ -51,6 +94,7 @@ public class Tutorial : MonoBehaviour
             {
                 popUpIndex++;
             }
+           
         }
 
 
