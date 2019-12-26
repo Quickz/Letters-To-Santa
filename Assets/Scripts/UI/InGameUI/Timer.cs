5dfc8410c36491c58070d98f04d420b3b8d7c616
Quickz <alexserg@inbox.lv>
2019-12-26 04:05:45 +0200

    re-added erased changes

--- Assets/Scripts/UI/InGameUI/Timer.cs
+++ Assets/Scripts/UI/InGameUI/Timer.cs
@@ -2,26 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
-    public float TimeLeft;
-    public WinScreen ws;
-    public FailScreen fs;
-    public Text tx;
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if (TimeLeft <= 0)
-        {
-            
-            ws.DoneGame();
-        } else
-        {
-            TimeLeft -= Time.deltaTime;
-        }
+    public float RemainingTime { get; private set; }
+    public float InitialTime => initialTime;
 
-        tx.text = "" + (int)TimeLeft;
+    [SerializeField]
+    private TMP_Text textField = null;
+
+    [SerializeField]
+    private Image progressBar = null;
+
+    [Tooltip("Amount of time left in the beginning (in seconds).")]
+    [SerializeField]
+    private float initialTime = 60f;
+
+    private void Awake()
+    {
+        RemainingTime = initialTime;
+    }
+
+    private void Start()
+    {
+        StartTimerAndOnCompletionExecute(WinScreen.Instance.DoneGame);
+    }
+
+    private void StartTimerAndOnCompletionExecute(Action callback)
+    {
+        StartCoroutine(Process());
+
+        IEnumerator Process()
+        {
+            while (RemainingTime > 0f)
+            {
+                RemainingTime -= Time.deltaTime;
+                
+                if (textField != null)
+                {
+                    textField.text = RemainingTime.ToString("0");
+                    progressBar.fillAmount = 1f / (initialTime / RemainingTime);
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+            callback();
+        }
     }
 }
