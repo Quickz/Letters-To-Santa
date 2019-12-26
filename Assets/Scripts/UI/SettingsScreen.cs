8b1cb42f666eb60bc3fcc54e827650092d9943fd
Quickz <alexserg@inbox.lv>
2019-12-26 02:32:28 +0200

    added a button for deleting saved data in the settings screen

--- Assets/Scripts/UI/SettingsScreen.cs
+++ Assets/Scripts/UI/SettingsScreen.cs
@@ -8,6 +8,9 @@ public class SettingsScreen : BaseScreen
     public static SettingsScreen Instance { get; private set; }
 
     [SerializeField]
+    private Button deleteSavesButton = null;
+
+    [SerializeField]
     private Button backButton = null;
 
     protected override void Awake()
@@ -26,6 +29,27 @@ public class SettingsScreen : BaseScreen
 
     private void Start()
     {
+        deleteSavesButton.onClick.AddListener(OnDeleteSavesButtonClicked);
         backButton.onClick.AddListener(MainScreen.Instance.OpenAlone);
+    }
+
+    public override void Close()
+    {
+        base.Close();
+        deleteSavesButton.interactable = true;
+    }
+
+    private void OnDeleteSavesButtonClicked()
+    {
+        DeleteSavedData();
+        deleteSavesButton.interactable = false;
+    }
+
+    private void DeleteSavedData()
+    {
+        SaveSystem.Clear();
+
+        // loading up the default values
+        SaveSystem.Load();
     }
 }
