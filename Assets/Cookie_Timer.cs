23857d483392a7c385016a039280edcf0cd93c17
JWoodrell <Joseph@Woodrell.com>
2019-12-26 20:12:57 -0500

    Changed timer to eating cookie

---
+++ Assets/Cookie_Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cookie_Timer : MonoBehaviour
+{
+    GameObject CookieParent;
+    [SerializeField]
+    float TimeLeft;
+    float InitialTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CookieParent = transform.parent.gameObject;
+        InitialTime = CookieParent.GetComponent<Timer>().InitialTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLeft = CookieParent.GetComponent<Timer>().RemainingTime / InitialTime;
+        gameObject.GetComponent<SpriteMask>().alphaCutoff = 1.0f - TimeLeft;
+    }
+}
